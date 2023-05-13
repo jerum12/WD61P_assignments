4dce07580e9d3e62808b64d540579b973f41368c
ClarizzeSanchez <130806616+ClarizzeSanchez@users.noreply.github.com>
2023-05-14 02:05:58 +0800

    Add files via upload

---
+++ JS_activities/CS_activity1.js
@@ -0,0 +1,13 @@
+const _bootcampName = "Kodego";
+const _maleStudents = 30;
+const _femaleStudents = 20;
+const _lecture = "Data Types & Variables";
+const _moreStudents = true;
+
+console.log("The type of bootcampName is " + typeof _bootcampName);
+console.log("The type of maleStudents is " + typeof _maleStudents);
+console.log("The type of femaleStudents is " + typeof _femaleStudents);
+console.log("The type of lecture is " + typeof _lecture);
+console.log("The type of moreStudents is " + typeof _moreStudents);
+
+console.log("The " + _bootcampName + " Bootcamp program enrolls mostly " + _maleStudents + " students. Students studying " + _lecture + " include " + _femaleStudents + " females & " + _maleStudents + " males.");
