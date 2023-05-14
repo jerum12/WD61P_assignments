68159b3f186502d0e6636a26c81a13359b88d08a
Julyza Pena <jbpena101@gmail.com>
2023-05-14 18:06:37 +0800

    updata to js exercise

---
+++ JS-Activity-1/app.js
@@ -0,0 +1,8 @@
+let bootcamp = 'Kodego'
+let maleStud = 30;
+let femaleStud = 20;
+let lecture = "Data Types & Variables";
+let moreMale = true;
+
+console.log("The " + bootcamp + " Bootcamp program enrolls mostly " + (maleStud+femaleStud) + " students.");
+console.log("Students studying " + lecture + " " + femaleStud + " females & " + maleStud + " males");
---
+++ JS-Activity-2/app.js
@@ -0,0 +1,24 @@
+let x = 2.5;
+let y = 3;
+if(x < 0) {
+    console.log(ans = (x / y) * x + 4);
+    console.log("condition 1");
+} else if (y < 0) {
+    console.log(ans = x - y);
+    console.log("condition 2");
+} else if(x > y) {
+    let z = ((x * 5) / 2) % y;
+    console.log(ans = z);
+    console.log("condition 3");
+} else {
+    let z = (x % 5) * (2 * y);
+    console.log(ans = z);
+    console.log("condition 4");
+}
+
+console.log("|  x  |  y  |  condition  |  ans  |");
+console.log("| 1.5 |  2  |      4      |   6   |");
+console.log("| -1  | -1  |      1      |   3   |");
+console.log("| 0.5 |-2.5 |      2      |   3   |");
+console.log("| 10  |  4  |      3      |   1   |");
+console.log("| 2.5 |  3  |      4      |  15   |");
