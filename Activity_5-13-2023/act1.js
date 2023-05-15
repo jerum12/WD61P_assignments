2f43fec6bf314c9c0b4b0e7d916ae81c74282d6e
vinaarellano <vinaagudo@gmail.com>
2023-05-15 19:46:03 +0800

    Excercise 1

---
+++ Activity_5-13-2023/act1.js
@@ -0,0 +1,15 @@
+let nameofbootcamp = 'Kodego';
+let malestudents = 30 ; 
+let femalestudents = 20 ; 
+let lectures = 'Data Types & Variables';
+let maleoverfemale = true ; 
+
+let gender = maleoverfemale ? 'male' : 'female';
+
+console.log (typeof nameofbootcamp);
+console.log (typeof malestudents)
+console.log (typeof femalestudents)
+console.log (typeof lectures)
+console.log (typeof maleoverfemale)
+
+console.log ('The '+ nameofbootcamp + ' Bootcamp program enrolls mostly ' + gender + ' students.\n' + 'Students studying ' + lectures + ' include ' + femalestudents + ' females & ' + malestudents + ' males.');
