0c33e26d2eff7abc50879b28d823f75fa67af200
vinaarellano <vinaagudo@gmail.com>
2023-05-17 04:20:45 +0800

    Activity 4

---
+++ Activity_5-13-2023/Activity4/scripts.js
@@ -0,0 +1,63 @@
+var presidents = [
+  {
+    name: 'Bongbong Marcos',
+    details: '2022-Present',
+  },
+  {
+    name: 'Rodrigo Duterte',
+    details: '2016-2012',
+  },
+  {
+    name: 'Benigno Aquino III',
+    details: '2010-2016',
+  },
+  {
+    name: 'Gloria Macapagal Arroyo',
+    details: '2001-2010',
+  },
+  {
+    name: 'Joseph Ejercito Estrada',
+    details: '1998-2001',
+  },
+  {
+    name: 'Fidel V. Ramos',
+    details: '1992-1998',
+  },
+  {
+    name: 'Corazon Aquino',
+    details: '1986-1992',
+  },
+  {
+    name: 'Ferdinand Marcos',
+    details: '1965-1986',
+  },
+  {
+    name: 'Diosdado Macapagal',
+    details: '1961-1965',
+  },
+];
+
+let imgVar = document.createElement('img');
+imgVar.setAttribute('src','logo.png');
+
+document.querySelector('#root').appendChild(imgVar);
+
+let divcon = document.createElement('div');
+  divcon.setAttribute('class','container');
+  document.querySelector('#root').appendChild(divcon);
+
+
+for (let i = 0; i < presidents.length; i++) {
+  
+  let divar = document.createElement('div');
+  divar.setAttribute('class','card');
+  let h1 = document.createElement('h1');
+  divar.appendChild(h1);
+  h1.textContent = presidents[i].name;
+  let p = document.createElement('p');
+  divar.appendChild(p);
+  p.textContent = presidents[i].details;
+  document.querySelector('.container').appendChild(divar);
+  
+}
+
