f328ecfa498b114d289b70b6854822b3bb35f521
Cyrelle12 <130672975+Cyrelle12@users.noreply.github.com>
2023-05-25 07:12:42 -0700

    Update JSFirstActivity.js

--- JSFirstActivity.js
+++ JSFirstActivity.js
@@ -2,14 +2,14 @@ const bootcamp = "Kodego";
 const maleStudent = 30;
 const femaleStudent = 20;
 const lecture = "Data Types and Variables";
-const AreThereMoreMaleThanFemale = true;
+const AreThereMoreMaleStudentsThanFemale = true;
 
 
 console.log(typeof(bootcamp));
 console.log(typeof(maleStudent));
 console.log(typeof(femaleStudent));
 console.log(typeof(lecture));
-console.log(typeof(AreThereMoreMaleThanFemale));
+console.log(typeof(AreThereMoreMaleStudentsThanFemale));
 
 
 console.log("The" + bootcamp + " Bootcamp program enrolls mostly " + "male Students." + "Student studying " + lecture + " include " + femaleStudent + " females & " + maleStudent + " males." )
