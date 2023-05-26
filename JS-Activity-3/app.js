c51eaa1e7b0b8e7aa3ebef1acbe874cc5914f409
Julyza Pena <jbpena101@gmail.com>
2023-05-26 19:52:07 +0800

    activity 3

--- JS-Activity-3/app.js
+++ JS-Activity-3/app.js
@@ -1,1 +1,15 @@
 let students = ['Elen', 'Sam', 'Piolo', 'Dingdong', 'Pia', 'Barbie', 'Jessie', 'Luis', 'Robi', 'Marian'];
+
+//1. using a loop, iterate throught this array and console.log all the students
+for(let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+
+//2. command to remove Robi from the array
+const removeRobi = students.findIndex(student => student === 'Robi');
+if(removeRobi > -1) {
+    students.splice(removeRobi, 1);
+}
+console.log(students);
+
+//3.
