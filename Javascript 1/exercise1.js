8b3e96d2f0f3ec6ac125e47fa1486ddee8fb6cdf
TiffyNikz <130542429+TiffyNikz@users.noreply.github.com>
2023-05-30 14:58:17 +0800

    Add files via upload

---
+++ Javascript 1/exercise1.js
@@ -0,0 +1,8 @@
+const b = "KodeGo";
+let m = 30;
+let f = 20;
+let x = "male";
+let l = "Data types and variables";
+
+document.getElementById("ex").innerHTML = "The" + " " + b + " " + "Bootcamp program enrolls mostly" + " " + x + " " +"students.";
+document.getElementById("way").innerHTML = "Students studying" + " " + l + " " +"include" + " " + f + " " + "females &" + " " + m + " " + "males.";
