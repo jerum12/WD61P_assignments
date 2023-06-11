0c2d0b8d5beb72c4d97c611d4a3f1aa6054f16c6
Julyza Pena <jbpena101@gmail.com>
2023-06-11 12:20:30 +0800

    JS Activity 4

--- JS-Activity-4/app.js
+++ JS-Activity-4/app.js
@@ -37,3 +37,41 @@ var presidents = [
     },
   ];
   
+let rootDiv = document.querySelector("#root");
+
+const createLogo = () => {
+  const imgLogo = document.createElement("img");
+  imgLogo.src = "logo.png";
+  imgLogo.style.width = "120px";
+
+  rootDiv.appendChild(imgLogo);
+}
+
+const createCardContainer = () => {
+  const cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("container");
+
+  const newCard = (presidentList) => {
+    for (let v in presidentList) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      cardsContainer.appendChild(card);
+
+      let presidentName = document.createElement("h1");
+      presidentName.classList.add("fs-5");
+      presidentName.innerHTML = presidentList[v].name;
+      card.appendChild(presidentName);
+
+      let dates = document.createElement("p");
+      dates.classList.add("text-center");
+      dates.innerHTML = presidentList[v].details + "...";
+      card.appendChild(dates);
+    }
+  }
+
+  newCard(presidents);
+  rootDiv.appendChild(cardsContainer);
+}
+
+createLogo();
+createCardContainer();
